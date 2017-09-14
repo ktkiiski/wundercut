4c95b6d5c03b4ea327751f820ceace9381ce1aa6
Kimmo Kiiski <kimmo@kii.ski>
2017-09-14 20:20:04 +0300

    Print the solution as a table

--- wundercut.py
+++ wundercut.py
@@ -2,8 +2,20 @@
 from __future__ import unicode_literals
 import json
 
-with open('./input.json', 'r') as input_file:
-    material = json.load(input_file)
+def get_optimal_cuts(material):
+    """
+    Finds and returns the optimal cuts for the given material (input data).
+    Returns a dict where keys are gemstone type and values are the list of optimal cut sizes.
+    """
+    return {'todo': [1,2,3]}
+
 
 if __name__ == '__main__':
-    print(json.dumps(material))
+    # Read the input data
+    with open('./input.json', 'r') as input_file:
+        material = json.load(input_file)
+    # Print the optimal cuts as an ASCII table
+    print("Gemstone  | Cuts")
+    print("----------|----------------")
+    for gemstone, cuts in get_optimal_cuts(material).items():
+        print("%9s | %s" % (gemstone, ", ".join(str(size) for size in cuts)))
