b19ad0cd28fe776a7ae0590dbef6eb1c1b742786
Kimmo Kiiski <kimmo@kii.ski>
2017-09-14 20:23:33 +0300

    Dummy solution function

--- wundercut.py
+++ wundercut.py
@@ -2,13 +2,24 @@
 from __future__ import unicode_literals
 import json
 
-def get_optimal_cuts(material):
+def get_solution(material):
     """
     Finds and returns the optimal cuts for the given material (input data).
     Returns a dict where keys are gemstone type and values are the list of optimal cut sizes.
     """
-    return {'todo': [1,2,3]}
+    return {
+        gemstone: get_optimal_cuts(info['cuts'], info['rawChunks'])
+        for gemstone, info in material.items()
+    }
 
+
+def get_optimal_cuts(cuts, chunks):
+    """
+    Determines the optimal chunks for the given gemstone type, by using
+    the given possible cut sizes/values and the raw chunks.
+    """
+    # TODO
+    return [1,2,3]
 
 if __name__ == '__main__':
     # Read the input data
@@ -17,5 +28,5 @@ if __name__ == '__main__':
     # Print the optimal cuts as an ASCII table
     print("Gemstone  | Cuts")
     print("----------|----------------")
-    for gemstone, cuts in get_optimal_cuts(material).items():
+    for gemstone, cuts in get_solution(material).items():
         print("%9s | %s" % (gemstone, ", ".join(str(size) for size in cuts)))
