983b8f3c613d7e739ec1a1ba2cebd6357d99573e
Kimmo Kiiski <kimmo@kii.ski>
2017-09-14 20:13:47 +0300

    Read input file

--- wundercut.py
+++ wundercut.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 from __future__ import unicode_literals
+import json
+
+with open('./input.json', 'r') as input_file:
+    material = json.load(input_file)
 
 if __name__ == '__main__':
-    print("TODO")
+    print(json.dumps(material))
